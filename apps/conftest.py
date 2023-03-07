75316630aa4eb6d29d6c748dab2b9067d6d14fef
Ricardo Ferrari <ricjferrari@gmail.com>
2023-03-07 06:33:54 -0300

    fix(tests): delete files generated by tests

--- apps/conftest.py
+++ apps/conftest.py
@@ -1,8 +1,21 @@
+import shutil
+
 from account.models import ProfileCandidate, ProfileCompany
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.messages.storage.fallback import FallbackStorage
 from model_bakery import baker
 from pytest import fixture
+
+
+@fixture(scope='session', autouse=True)
+def media_root(tmpdir_factory):
+    """
+    Override Django media root with a temporary directory.
+    """
+    settings.MEDIA_ROOT = tmpdir_factory.mktemp('media', numbered=True)
+    yield settings.MEDIA_ROOT
+    shutil.rmtree(settings.MEDIA_ROOT, ignore_errors=True)
 
 
 @fixture
