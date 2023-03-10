f791295368b3e327f269eda108c5e07709b5edb6
Ricardo Ferrari <ricjferrari@gmail.com>
2023-03-10 20:46:55 -0300

    fix(settings): fix environment variable cast

--- core/settings.py
+++ core/settings.py
@@ -186,7 +186,7 @@ EMAIL_BACKEND = config(
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST = config('EMAIL_HOST')
 
 MAX_UPLOAD_SIZE = 2621440   # 2.5MB
